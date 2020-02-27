e4b705c9d2a3f2e29faab52448b30a5a72e876b0
Anisha KC <anisha.kc7219@gmail.com>
2020-02-27 15:24:41 -0500

    Add a Cancel button to the 'Add Card Screen'

---
+++ app/src/main/java/com/example/akc/flashcardapp/AddCardActivity.java
@@ -0,0 +1,21 @@
+package com.example.akc.flashcardapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_card_activity);
+
+        findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
