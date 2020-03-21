70f068ea1b62b5e32ecac394256d767f9b661049
Anisha KC <anisha.kc7219@gmail.com>
2020-03-21 13:50:33 -0400

    All optionals for lab3 done

--- app/src/main/java/com/example/akc/flashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/akc/flashcardapp/AddCardActivity.java
@@ -14,10 +14,14 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_card_activity);
 
-        String s1 = getIntent().getStringExtra("currQuestion"); // this string will be 'harry potter`
-        String s2 = getIntent().getStringExtra("currAnswer"); // this string will be 'voldemort'
+        String s1 = getIntent().getStringExtra("currQuestion");
+        String s2 = getIntent().getStringExtra("currAnswer");
+        String s3 = getIntent().getStringExtra("newOption1");
+        String s4 = getIntent().getStringExtra("newOption2");
         ((EditText)findViewById(R.id.NewQuestion)).setText(s1);
         ((EditText)findViewById(R.id.NewAnswer)).setText(s2);
+        ((EditText)findViewById(R.id.wrongAnswer)).setText(s3);
+        ((EditText)findViewById(R.id.otherWrongAsnwer)).setText(s4);
 
         findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/akc/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/akc/flashcardapp/MainActivity.java
@@ -18,47 +18,70 @@ public class MainActivity extends AppCompatActivity {
         return rand.nextInt((maxNumber - minNumber) + 1) + minNumber;
     }
 
+
+    // -------------------- Just a function to make options invisible
     public void makeOptionsInvisible(){
         findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
         findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
-        ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.see);
-        findViewById(R.id.option1).setVisibility(View.INVISIBLE);
-        findViewById(R.id.option2).setVisibility(View.INVISIBLE);
-        findViewById(R.id.rightAnswer).setVisibility(View.INVISIBLE);
-        isShowingAnswers = true;
     }
 
+    // -------------------- Reset the answers color to default
+    public void resetOptionColors(){
+        findViewById(R.id.rightAnswer).setBackgroundColor(getResources().getColor(R.color.answerColor));
+        findViewById(R.id.option1).setBackgroundColor(getResources().getColor(R.color.answerColor));
+        findViewById(R.id.option2).setBackgroundColor(getResources().getColor(R.color.answerColor));
+    }
+
+    // -------------------- Variables
+    int ADD_CARD_REQUEST_CODE = 100;
+    int EDIT_CARD_REQUEST_CODE = 200;
     boolean isShowingAnswers = false;
     FlashcardDatabase flashcardDatabase;
+    Flashcard flashcardToEdit;
     List<Flashcard> allFlashcards;
 
+
+    // -------------------- onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Database to store our flashcards
         flashcardDatabase = new FlashcardDatabase(getApplicationContext());
         allFlashcards = flashcardDatabase.getAllCards();
         makeOptionsInvisible();
+        resetOptionColors();
 
+        // Populate
         if (allFlashcards != null && allFlashcards.size() > 0) {
             ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
             ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+            ((TextView) findViewById(R.id.rightAnswer)).setText(allFlashcards.get(0).getAnswer());
+            ((TextView) findViewById(R.id.option1)).setText(allFlashcards.get(0).getWrongAnswer1());
+            ((TextView) findViewById(R.id.option2)).setText(allFlashcards.get(0).getWrongAnswer2());
         }
 
+        // Go to the next question
         findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (allFlashcards.isEmpty()){
                     return;
                 } else {
-                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(getRandomNumber(0,allFlashcards.size() - 1)).getQuestion());
-                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(getRandomNumber(0,allFlashcards.size() - 1)).getAnswer());
+                    Flashcard temp = allFlashcards.get(getRandomNumber(0,allFlashcards.size() - 1));
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(temp.getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(temp.getAnswer());
+                    ((TextView) findViewById(R.id.rightAnswer)).setText(temp.getAnswer());
+                    ((TextView) findViewById(R.id.option1)).setText(temp.getWrongAnswer1());
+                    ((TextView) findViewById(R.id.option2)).setText(temp.getWrongAnswer2());
                     makeOptionsInvisible();
+                    resetOptionColors();
                 }
             }
         });
 
+        // Click question -> Turn the card to show answer
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Click answer -> Turn the card to show question
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,20 +99,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Clicking options (Red for wrong answers and green for right answers)
         findViewById(R.id.option1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.option1).setBackgroundColor(getResources().getColor(R.color.wrongAnswer));
             }
         });
-
         findViewById(R.id.option2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.option2).setBackgroundColor(getResources().getColor(R.color.wrongAnswer));
             }
         });
-
         findViewById(R.id.rightAnswer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,16 +119,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Edit the question
         findViewById(R.id.edit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int i = 0;
+                while (!allFlashcards.get(i).getQuestion().equals(((TextView) findViewById(R.id.flashcard_question)).getText().toString())){
+                    i++;
+                }
+                flashcardToEdit = allFlashcards.get(i);
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 intent.putExtra("currQuestion", ((TextView) findViewById(R.id.flashcard_question)).getText().toString());
                 intent.putExtra("currAnswer", ((TextView) findViewById(R.id.flashcard_answer)).getText().toString());
-                MainActivity.this.startActivityForResult(intent, 100);
+                intent.putExtra("newOption1", ((TextView) findViewById(R.id.option1)).getText().toString());
+                intent.putExtra("newOption2", ((TextView) findViewById(R.id.option2)).getText().toString());
+                MainActivity.this.startActivityForResult(intent, EDIT_CARD_REQUEST_CODE);
             }
         });
 
+        // Delete a question
         findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -114,14 +146,23 @@ public class MainActivity extends AppCompatActivity {
                 if (allFlashcards.isEmpty()){
                     ((TextView) findViewById(R.id.flashcard_question)).setText("Add a question!");
                     ((TextView) findViewById(R.id.flashcard_answer)).setText("Add a question and it's answer!");
+                    ((TextView) findViewById(R.id.rightAnswer)).setText("Add the answer!");
+                    ((TextView) findViewById(R.id.option1)).setText("Add a wrong option!");
+                    ((TextView) findViewById(R.id.option2)).setText("Add another wrong option!");
                 } else {
-                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(getRandomNumber(0,allFlashcards.size() - 1)).getQuestion());
-                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(getRandomNumber(0,allFlashcards.size() - 1)).getAnswer());
+                    Flashcard temp = allFlashcards.get(getRandomNumber(0,allFlashcards.size() - 1));
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(temp.getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(temp.getAnswer());
+                    ((TextView) findViewById(R.id.rightAnswer)).setText(temp.getAnswer());
+                    ((TextView) findViewById(R.id.option1)).setText(temp.getWrongAnswer1());
+                    ((TextView) findViewById(R.id.option2)).setText(temp.getWrongAnswer2());
                     makeOptionsInvisible();
+                    resetOptionColors();
                 }
             }
         });
 
+        // Click the eye icon to show or hide options
         findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -141,36 +182,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Add a new card
         findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivityForResult(intent, 100);
+                MainActivity.this.startActivityForResult(intent, ADD_CARD_REQUEST_CODE);
             }
         });
 
     }
 
+
+    // -------------------- On Activity for adding/editing a card
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 100) {
-            if(data != null){
-                Snackbar.make(findViewById(R.id.flashcard_question),
-                        "Card successfully created",
-                        Snackbar.LENGTH_SHORT)
-                        .show();
-                String question = data.getExtras().getString("newQuestion");
-                String answer = data.getExtras().getString("newAnswer");
-                String option1 = data.getExtras().getString("newOption1");
-                String option2 = data.getExtras().getString("newOption2");
-                flashcardDatabase.insertCard(new Flashcard(question, answer));
-                allFlashcards = flashcardDatabase.getAllCards();
-                ((TextView)findViewById(R.id.flashcard_question)).setText(question);
-                ((TextView)findViewById(R.id.flashcard_answer)).setText(answer);
-                ((TextView)findViewById(R.id.option1)).setText(option1);
-                ((TextView)findViewById(R.id.option2)).setText(option2);
-                ((TextView)findViewById(R.id.rightAnswer)).setText(answer);
-            }
+        if (requestCode == ADD_CARD_REQUEST_CODE && resultCode == RESULT_OK) {
+            Snackbar.make(findViewById(R.id.flashcard_question),
+                    "Card successfully created",
+                    Snackbar.LENGTH_SHORT)
+                    .show();
+            String question = data.getExtras().getString("newQuestion");
+            String answer = data.getExtras().getString("newAnswer");
+            String option1 = data.getExtras().getString("newOption1");
+            String option2 = data.getExtras().getString("newOption2");
+            flashcardDatabase.insertCard(new Flashcard(question, answer, option1, option2));
+            allFlashcards = flashcardDatabase.getAllCards();
+        } else if (requestCode == EDIT_CARD_REQUEST_CODE && resultCode == RESULT_OK) {
+            String question = data.getExtras().getString("newQuestion");
+            String answer = data.getExtras().getString("newAnswer");
+            String option1 = data.getExtras().getString("newOption1");
+            String option2 = data.getExtras().getString("newOption2");
+            flashcardToEdit.setQuestion(question);
+            flashcardToEdit.setAnswer(answer);
+            flashcardToEdit.setWrongAnswer1(option1);
+            flashcardToEdit.setWrongAnswer2(option2);
+            flashcardDatabase.updateCard(flashcardToEdit);
         }
     }
 }
